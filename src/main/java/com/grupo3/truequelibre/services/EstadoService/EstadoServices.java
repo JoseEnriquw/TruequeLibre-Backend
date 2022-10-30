4e9fb36d21008a3efdd9eca427fa9a75d9ad5269
JoseEnriquw <63372900+JoseEnriquw@users.noreply.github.com>
2022-10-30 18:23:06 -0300

    Implementación de validations, falta configurar el response de cuando valida

--- src/main/java/com/grupo3/truequelibre/services/EstadoService/EstadoServices.java
+++ src/main/java/com/grupo3/truequelibre/services/EstadoService/EstadoServices.java
@@ -5,15 +5,16 @@ import com.grupo3.truequelibre.tools.Response;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 import com.grupo3.truequelibre.dao.IEstadoDao;
 import com.grupo3.truequelibre.entity.*;
 import com.grupo3.truequelibre.interfaces.IEstadoServices;
 
 @Service
+@Validated
 public class EstadoServices implements IEstadoServices {
     
 	@Autowired
