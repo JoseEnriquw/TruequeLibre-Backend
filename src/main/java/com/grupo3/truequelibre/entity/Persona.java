c3779418ca5b2d0a0c18638473d365da6e2c7c57
JoseEnriquw <63372900+JoseEnriquw@users.noreply.github.com>
2022-11-13 20:48:25 -0300

    correciones, funciona de una vez!!!!

--- src/main/java/com/grupo3/truequelibre/entity/Persona.java
+++ src/main/java/com/grupo3/truequelibre/entity/Persona.java
@@ -6,13 +6,10 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-
 import org.hibernate.annotations.Type;
-import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name="persona")
