2c843f365bdcf4a655d772d1142547a07048cba5
JoseEnriquw <63372900+JoseEnriquw@users.noreply.github.com>
2022-11-15 23:43:20 -0300

    correcion!!

--- src/main/java/com/grupo3/truequelibre/dao/IPublicacionDao.java
+++ src/main/java/com/grupo3/truequelibre/dao/IPublicacionDao.java
@@ -14,7 +14,7 @@ public interface IPublicacionDao extends JpaRepository<Publicacion,Integer>{
 	@Query(value="select * from publicacion where categoria_id = :categoria and estado_id != 2 and estado_id != 6 and estado_id != 5 and usuario_id != :usuario",nativeQuery=true)
 	List<Publicacion> findByCategoria(Integer categoria, Integer usuario);
 	
-	@Query(value="select pu.* from publicacion pu inner join usuario usu on usu.id=pu.usuario_id inner join persona per on usu.dni=per.dni where (per.nombre like %:nombre% or per.apellido like %:nombre% or pu.nombre like %:nombre% or pu.descripcion like %:nombre%) and pu.categoria_id=:categoria and pu.usuario_id!=:usuario and pu.estado_id <>2 and pu.estado_id <> 6", nativeQuery=true)
+	@Query(value="select pu.* from publicacion pu inner join usuario usu on usu.id=pu.usuario_id inner join persona per on usu.dni=per.dni where (per.nombre like %:nombre% or per.apellido like %:nombre% or pu.nombre like %:nombre% or pu.descripcion like %:nombre%) and pu.categoria_id=:categoria and pu.usuario_id!=:usuario and pu.estado_id <>2 and pu.estado_id <> 6 and pu.estado_id <> 5", nativeQuery=true)
 	List<Publicacion> findByCategoriaIdNombreAndUsuarioIdNotIncompleto(Integer categoria,String nombre,Integer usuario);
 	
 	@Query(value="select * from publicacion where estado_id = :estado",nativeQuery=true)
